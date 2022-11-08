c24988efbbe50a0514355ef343b3903e9ebfadad
Markus Persson <markusperssonbiz@gmail.com>
2022-11-08 16:42:22 +0100

    Make slide in faster

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from "react";
 import { IFooter, IHeader } from "../interfaces";
-import Footer from "./Footer";
-import Header from "./Header";
+import dynamic from "next/dynamic";
+const Header = dynamic(() => import("./Header"), { ssr: false });
+const Footer = dynamic(() => import("./Footer"), { ssr: false });
 
 type Props = {
   children?: ReactNode;
